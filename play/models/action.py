5f357dfd2e8fc5475237c7d32e5a0a0d6791e421
youknow <choi15ho@gmail.com>
2023-06-03 03:38:05 +0900

    액션 관련 코드 추가

--- play/models/action.py
+++ play/models/action.py
@@ -18,74 +18,29 @@ class Action(Base):
         self._card = card_number
         self._turn = turn
 
-    # "
-    def run(self, players: List[Player], common_resources: Resource, turn: int, card_number: str):
-        # command: str = self._card.get("command")
-        # command = """
-        #     require("wood", 1), require("clay", 1), add_effect(["self.action == CARD_005"], ["player.get('resources').set('vegetable', player.get('resources').get('vegetable') + 1)"])
-        # """
+    def run(self, players, round_card):
+        player = players[self._turn]
 
-        # 누적 행동칸: 양시장, 소시장, 서부 채석장 등등...
         command = """
-            add("cattle", common_resources.get('cattle'))
+            add("resource", "amount")
         """
-
-        # 노누적 행동칸: 채소종자
-        command = """
-            add("vegetable", 1)
-        """
-
-        # 추가 행동이 필요한 행동칸: 울타리치기, 주요설비
-        command = """
-            False
-        """
-
-        # 추가 행동이 필요한 행동칸 + 뭔가의 행동이 추가: 밭 일구기 + 씨 뿌리기,
-        command = """
-            self._player.create_farm(position)
-        """
-        command.replace('position', "[0, 0]")
-
-        # 추가 행동이 필요 없는 칸인데, return True인 행동을 사용하는 경우: 단순 밭 일구기
-        command = """
-            self._player.create_farm(position), False
-        """
-        command.replace('position', "[0, 0]")
-
-        # 씨 뿌리기
-        command = """
-            require(resource, count), self._player.change_field_is_in(position, resource, count)
-        """
-        command.replace('resource', 'grain')
-        command.replace('count', '1')
-        command.replace('position', "[0, 0]")
-
-        # 농장 확장
-        command = """
-        """
-
-        # 집 개조, 농장 개조
-        command = """
-            require(resource, count), require(resource, count), self._player.remodeling(self._player)
-        """
-        command.replace('resource', 'reed')
-        command.replace('count', '1')
-        if self._player.get("_room_type") == "WOOD":
-            command.replace('resource', 'wood')
-            command.replace('count', 'self._field.get')
+        command.replace("resource", round_card.get("resource"))
+        command.replace("amount", round_card.get("count"))
 
         is_done = all(eval(command))
 
         pass
 
     # 카드를 낼 때 필요한 자원을 가져간다. -> 필요한 자원을 내는 것 뿐이므로 턴이 유지되는 것은 아님
-
-    def require(self, resource: str, value: int) -> bool:
-        if self._player.get("resources").get(resource) < value:
+    def require(self, player, resource: str, amount: int) -> bool:
+        if player.get("resources").get(resource) < amount:
             raise Exception("자원이 부족합니다.")
-        self._player.get("resources").set(resource, self._player.get("resources").get(resource) - value)
+        player.get("resources").set(resource, player.get("resources").get(resource) - amount)
         return True
 
-    def add(self, resource: str, value: int) -> bool:
-        self._player.get("resources").set(resource, self._player.get("resources").get(resource) + value)
+    # add
+    # 자원을 추가한다.
+    def add(self, player, resource: str, amount: int) -> bool:
+        player.get("resources").set(resource, player.get("resources").get(resource) + amount)
         return True
+
