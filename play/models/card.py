959db900e73477660b22e6601d2d1e1e8f6a02af
sunsuking <sunsuking@gmail.com>
2023-06-03 22:28:07 +0900

    카드 받아올 것 추가

--- play/models/card.py
+++ play/models/card.py
@@ -10,25 +10,13 @@ class Card(Base):
     def __init__(
             self,
             card_number: str,
-            card_type: str,
             name: str,
-            image: str,
-            cost: dict,
             score: int,
-            condition: str,
-            command: str,
-            description: str,
             is_use: bool = False
     ):
         self._card_number = card_number
-        self._card_type = card_type
         self._name = name
-        self._image = image
-        self._cost = cost
         self._score = score
-        self._condition = condition
-        self._command = command
-        self._description = description
         self._is_use = is_use
 
     # card 데이터베이스로부터 카드 정보를 가져온다.
